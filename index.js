265b445b83eeab0845ea860a0435f2a9eef0fd3b
Eduarda <duda.eduarda.ec@gmail.com>
2023-04-24 14:24:53 -0300

    substituindo body-parser

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(cors())
 
